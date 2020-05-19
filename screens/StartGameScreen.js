84c77f66db553638d8ef6a4ece21bdd06aff2010
alshaboti <alshaboti.it@gmail.com>
2020-05-20 10:30:36 +1200

    initial game layout

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+
+const StartGameScreen = props => {
+    return (
+
+        <View style={styles.screen}>
+            <Text style={styles.title}> Some text</Text>
+            <View style={styles.inputContainer}>
+                <Text> Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => { }} />
+                    <Button title="Confirm" onPress={() => { }} />
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        // only for ios
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        // for android
+        elevation: 5,
+        padding: 10,
+        borderRadius: 5
+
+
+
+
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    }
+});
+
+export default StartGameScreen;
